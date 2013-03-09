be7429deebe6f97bc71ef3960513e569b8964b5b
Damiano <damianofds@gmail.com>
2013-03-09 14:35:05 +0100

    #52 add a method to Utils.java class

--- webapp/src/main/java/it/geosolutions/fra2015/tags/Utils.java
+++ webapp/src/main/java/it/geosolutions/fra2015/tags/Utils.java
@@ -1,10 +1,13 @@
 package it.geosolutions.fra2015.tags;
 
-import javax.servlet.jsp.PageContext;
-
 import it.geosolutions.fra2015.mvc.controller.utils.ControllerServices.OperationWR;
 
+import org.apache.log4j.Logger;
+
 public class Utils {
+    
+    private static final Logger LOGGER = Logger.getLogger(Utils.class);
+    
 	static OperationWR validateOperation(String operation) {
 
 		OperationWR op = null;
@@ -19,4 +22,15 @@ public class Utils {
 		return op;
 	}
 	
+	public static boolean genericEnumValidator(Class enumerator, String valueToCheck){
+	        
+	        try{
+	            Enum.valueOf(enumerator, valueToCheck);
+	        }
+	        catch (Exception e){
+	            LOGGER.error("The value '" + valueToCheck +"' don't match with domain specified by " + enumerator);
+	            return false;
+	        }
+	        return true;
+	    } 
 }
