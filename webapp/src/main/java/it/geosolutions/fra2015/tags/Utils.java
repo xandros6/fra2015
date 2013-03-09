9b7c8a4e425e7999671cdb64aa6ec963c392aac4
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2013-03-09 01:34:50 +0100

    improved behiviour of richtext entry

--- webapp/src/main/java/it/geosolutions/fra2015/tags/Utils.java
+++ webapp/src/main/java/it/geosolutions/fra2015/tags/Utils.java
@@ -1,21 +1,22 @@
 package it.geosolutions.fra2015.tags;
 
+import javax.servlet.jsp.PageContext;
+
 import it.geosolutions.fra2015.mvc.controller.utils.ControllerServices.OperationWR;
 
 public class Utils {
-static OperationWR validateOperation(String operation){
-        
-        OperationWR op = null;
-        if(operation == null || operation.isEmpty()){
-            return OperationWR.WRITE;
-        }
-        try{
-            op = OperationWR.valueOf(operation);
-        }
-        catch(Exception e){
-            return null;
-        }
-        return op;
-    }
+	static OperationWR validateOperation(String operation) {
 
+		OperationWR op = null;
+		if (operation == null || operation.isEmpty()) {
+			return OperationWR.WRITE;
+		}
+		try {
+			op = OperationWR.valueOf(operation);
+		} catch (Exception e) {
+			return null;
+		}
+		return op;
+	}
+	
 }
