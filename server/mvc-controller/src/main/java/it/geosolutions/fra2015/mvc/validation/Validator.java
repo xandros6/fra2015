200e0af32586e84db3ac53c64c708a344d0c56ab
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2013-03-13 01:09:19 +0100

    add support to static rules validation. #83

--- server/mvc-controller/src/main/java/it/geosolutions/fra2015/mvc/validation/Validator.java
+++ server/mvc-controller/src/main/java/it/geosolutions/fra2015/mvc/validation/Validator.java
@@ -1,7 +1,6 @@
 package it.geosolutions.fra2015.mvc.validation;
 
 import it.geosolutions.fra2015.server.model.survey.Country;
-import it.geosolutions.fra2015.server.model.survey.EntryItem;
 import it.geosolutions.fra2015.server.model.survey.Value;
 import it.geosolutions.fra2015.services.SurveyService;
 import it.geosolutions.fra2015.services.exception.BadRequestServiceEx;
@@ -10,7 +9,6 @@ import it.geosolutions.fra2015.validation.ValidationMessage;
 import it.geosolutions.fra2015.validation.ValidationResult;
 import it.geosolutions.fra2015.validation.ValidationRule;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -20,6 +18,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import javax.script.ScriptException;
 import javax.xml.bind.JAXB;
 
 import org.slf4j.Logger;
@@ -29,223 +28,353 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
 
+/**
+ * Check some conditions provided from a configuration file Check the variables in the database and provide validation result messages for each rule.
+ * 
+ * @author Lorenzo Natali
+ * 
+ */
 public class Validator implements InitializingBean, ApplicationContextAware {
-	private ApplicationContext applicationContext;
-	private final static Logger LOGGER = LoggerFactory
-			.getLogger(Validator.class);
+    private ApplicationContext applicationContext;
 
-	
-	RuleList ruleList;
-	@Autowired
-	private SurveyService surveyService;
+    private final static Logger LOGGER = LoggerFactory.getLogger(Validator.class);
 
-	/**
-	 * Validates the whole country values
-	 * 
-	 * @param country
-	 * @return
-	 */
-	public ValidationResult validate(String country) {
+    RuleList ruleList;
 
-		Iterator<ValidationRule> it = ruleList.iterator();
-		ValidationResult result = new ValidationResult();
-		result.setSuccess(true);
-		// get the needed variable names (1.1 ...)
-		HashSet<String> varnames = new HashSet<String>();
-		while (it.hasNext()) {
-			ValidationRule rule = it.next();
-			varnames.addAll(rule.getVariables());
+    @Autowired
+    private SurveyService surveyService;
 
-		}
-		// get external variables
-		Country c = surveyService.searchCountry(country);
-		Map<String, String> externals = new HashMap<String, String>();
-		if (c.getCountryArea() != null && c.getLandArea() != null) {
-			externals.put("COUNTRY_AREA", c.getCountryArea() + "");
-			externals.put("LAND_AREA", c.getLandArea() + "");
-		}
+    /**
+     * Validates the whole country values
+     * 
+     * @param country
+     * @return
+     */
+    public ValidationResult validate(String country) {
 
-		try {
-			// get variable values
-			List<Value> values = surveyService.getEntryListByVariableName(
-					new ArrayList<String>(varnames), country);
-			// validate the rules
-			it = ruleList.iterator();
-			while (it.hasNext()) {
-				ValidationRule rule = it.next();
-				validateRule(values, rule, result, externals);
+        Iterator<ValidationRule> it = ruleList.iterator();
+        ValidationResult result = new ValidationResult();
+        result.setSuccess(true);
+        // get the needed variable names (1.1 ...)
+        HashSet<String> varnames = new HashSet<String>();
+        while (it.hasNext()) {
+            ValidationRule rule = it.next();
+            varnames.addAll(rule.getVariables());
 
-			}
-		} catch (BadRequestServiceEx e) {
-			LOGGER.error("error retriving variables during validation", e);
+        }
+        // get external variables
+        Country c = surveyService.searchCountry(country);
+        Map<String, String> externals = new HashMap<String, String>();
+        if (c.getCountryArea() != null && c.getLandArea() != null) {
+            externals.put("COUNTRY_AREA", c.getCountryArea() + "");
+            externals.put("LAND_AREA", c.getLandArea() + "");
+        }
 
-		}
+        try {
+            // get variable values
+            List<Value> values = surveyService.getEntryListByVariableName(new ArrayList<String>(
+                    varnames), country);
+            calculateVariations(externals, values);
+            // validate the rules
+            it = ruleList.iterator();
 
-		return result;
-	}
-	/**
-	 * Validate a single rule for every year
-	 * @param values
-	 * @param rule
-	 * @param result
-	 * @param externals
-	 */
-	private void validateRule(List<Value> values, ValidationRule rule,
-			ValidationResult result, Map<String, String> externals) {
-		Map<String, Map<String, String>> tests = new HashMap<String, Map<String, String>>();
-		// get all columns (1990 2000 etc...)
-		if (values.size() == 0) {
-			result.setSuccess(false);
-			ValidationMessage v = new ValidationMessage();
-			v.setMessage("validation.notcompiled");
-			v.addElement(rule.getEntryId());
-			result.addMessage(v);
-			return;
-		}
-		for (Value value : values) {
-			// for each column add a map name (1.2) -> value (e.g. 1234)
-			Map<String, String> testEntry;
-			String colname = value.getEntryItem().getColumnName();
-			if (!tests.containsKey(colname)) {
-				testEntry = new HashMap<String, String>();
-				tests.put(colname, testEntry);
-			} else {
-				testEntry = tests.get(colname);
-			}
-			testEntry
-					.put(value.getEntryItem().getRowName(), value.getContent());
-		}
-		// for each column (year)
-		ValidationMessage message = null;
-		// some validation problems have priority
-		boolean alreadyChecked = false;
-		for (String key : tests.keySet()) {
-			if (alreadyChecked) {
-				continue;
-			}
+            while (it.hasNext()) {
+                ValidationRule rule = it.next();
+                validateRule(values, rule, result, externals);
 
-			// get the map name ->value
-			Map<String, String> test = tests.get(key);
-			List<String> missing = checkRuleFields(rule, test);
-			// missing variables
-			if (missing.size() > 0) {
-				message = new ValidationMessage();
-				message.setMessage("validation.notcompiled");
-				message.setRule(rule);
-				message.setSuccess(false);
-				message.setElements(missing);
-				result.setSuccess(false);
-				alreadyChecked = true;
-				continue;
-			}
+            }
+        } catch (BadRequestServiceEx e) {
+            LOGGER.error("error retriving variables during validation", e);
 
-			try {
-				boolean success = rule.evaluate(test, externals);
-				//
-				// check rules
-				//
+        }
 
-				// create the message if not created
-				if (message == null) {
-					message = new ValidationMessage();
-					message.setRule(rule);
-					message.setSuccess(success);
-					message.setMessage(rule.getError());
-				}
-				if (!success) {
-					// add the columns that not satisfy the rule
-					
-					message.addElement(key);
-					message.setSuccess(false);
-					result.setSuccess(false);
-				}
-			} catch (ScriptException e) {
-				generateParseProblemMessage(message,rule);
-				result.setSuccess(false);
-				alreadyChecked = true;
+        return result;
+    }
 
-			} catch (NullPointerException e) {
-			        generateParseProblemMessage(message,rule);
-				result.setSuccess(false);
-				alreadyChecked = true;
-			}
+    /**
+     * calculate change rate and net change of forests not year dependent
+     * 
+     * @param externals
+     * @param values
+     */
+    private void calculateVariations(Map<String, String> externals, List<Value> values)
+            throws IllegalArgumentException {
+        //
+        // Change Rate
+        //
 
-		}
-		result.addMessage(message);
-
-	}
-
-	private void generateParseProblemMessage(ValidationMessage message, ValidationRule rule) {
-	    message = new ValidationMessage();
-            message.setMessage("validation.parseproblem");
-            message.setRule(rule);
-            message.setSuccess(false);
-            message.addElement(rule.getCondition());
+        // get forest Areas
+        Map<String, Value> forestAreas = new HashMap<String, Value>();
+        //1.1
+        for (Value v : values) {
+            if ("1.1".equals(v.getEntryItem().getRowName())) {
+                forestAreas.put(v.getEntryItem().getColumnName(), v);
+            }
+        }
+        double area90 = Double.parseDouble(forestAreas.get("1990").getContent());
+        double area00 = Double.parseDouble(forestAreas.get("2000").getContent());
+        double area05 = Double.parseDouble(forestAreas.get("2005").getContent());
+        // Change Rate 1990-2000 = (( (1.1_2000)/(1.1_1990) )^0.1-1)*100
+        // A1= Forest area 1990 (1.1)(1990)
+        // A2= Forest area 2000 (1.1)(2000)
+        double changerate1990_2000 = (Math.pow(area00 / area90, 0.1) - 1) * 100;
+        // Change Rate 2000-2005= (((1.1_2005)/(1.1_2000))^0.2-1)*100
+        // A1= Forest area 2000 (1.1) (2000)
+        // A2= Forest area 2005 (1.1) (2005)
+        double changerate2000_2005 = (Math.pow(area05 / area00, 0.2) - 1) * 100;
+        // Change Rate 1990-2005= (((1.1_2005)/(1.1_1990))^0.06667-1)*100
+        // A1= Forest area 1990 (1.1) (1990)
+        // A2= Forest area 2005 (1.1) (2005)
+        double changerate1990_2005 = (Math.pow(area05 / area00, 0.06667) - 1) * 100;
+        externals.put("CHR11_90_00", changerate1990_2000 + "");
+        externals.put("CHR11_00_05", changerate2000_2005 + "");
+        externals.put("CHR11_90_05", changerate1990_2005 + "");
+        for (Value v : values) {
+            if ("2.1".equals(v.getEntryItem().getRowName())) {
+                forestAreas.put(v.getEntryItem().getColumnName(), v);
+            }
+        }
+        //2.1
+        area90 = Double.parseDouble(forestAreas.get("1990").getContent());
+        area00 = Double.parseDouble(forestAreas.get("2000").getContent());
+        area05 = Double.parseDouble(forestAreas.get("2005").getContent());
+        // Change Rate 1990-2000 = (( (1.1_2000)/(1.1_1990) )^0.1-1)*100
+        // A1= Forest area 1990 (1.1)(1990)
+        // A2= Forest area 2000 (1.1)(2000)
+        changerate1990_2000 = (Math.pow(area00 / area90, 0.1) - 1) * 100;
+        // Change Rate 2000-2005= (((1.1_2005)/(1.1_2000))^0.2-1)*100
+        // A1= Forest area 2000 (1.1) (2000)
+        // A2= Forest area 2005 (1.1) (2005)
+        changerate2000_2005 = (Math.pow(area05 / area00, 0.2) - 1) * 100;
+        // Change Rate 1990-2005= (((1.1_2005)/(1.1_1990))^0.06667-1)*100
+        // A1= Forest area 1990 (1.1) (1990)
+        // A2= Forest area 2005 (1.1) (2005)
+        changerate1990_2005 = (Math.pow(area05 / area00, 0.06667) - 1) * 100;
+        externals.put("CHR21_90_00", changerate1990_2000 + "");
+        externals.put("CHR21_00_05", changerate2000_2005 + "");
+        externals.put("CHR21_90_05", changerate1990_2005 + "");
         
     }
-    private List<String> checkRuleFields(ValidationRule rule,
-			Map<String, String> test) {
-		List<String> missing = new ArrayList<String>();
-		List<String> vars = rule.getVariables();
-		for (String name : vars) {
-			;
-			if (test.get(name) == null) {
-				missing.add(name);
-			}
-		}
-		return missing;
-	}
 
-	private Map createMap() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Validate a single rule for every year
+     * 
+     * @param values
+     * @param rule
+     * @param result
+     * @param externals
+     */
+    private void validateRule(List<Value> values, ValidationRule rule, ValidationResult result,
+            Map<String, String> externals) {
+        Map<String, Map<String, String>> tests = new HashMap<String, Map<String, String>>();
+        // check if there is no value
+        if (values.size() == 0) {
+            result.setSuccess(false);
+            ValidationMessage v = new ValidationMessage();
+            v.setMessage("validation.notcompiled");
+            v.addElement(rule.getEntryId());
+            result.addMessage(v);
+            return;
+        }
+        // check single rule (dosn't contains variables
+        // e.g. change rate1 in % should be between +3.00% and -3.00%
+        if (rule.getVariables().size() == 0) {
 
-	public RuleList getRuleList() {
-		return ruleList;
-	}
+            evaluateSingleRule(result, rule, externals);
+            return;
+        }
+        // get all columns (1990 2000 etc...)
+        populateTests(values, tests);
+        evaluateColumns(result, tests, externals, rule);
+        // for each column (year)
 
-	public void setRuleList(RuleList ruleList) {
-		this.ruleList = ruleList;
-	}
+    }
+    /** evaluate rules not column dependent
+     * @param result
+     * @param rule
+     * @param externals
+     */
+    private void evaluateSingleRule(ValidationResult result, ValidationRule rule,
+            Map<String, String> externals) {
+        // check if some rule is missing
+        List<String> missing = checkRuleFields(rule.getExternalData(), externals);
+        ValidationMessage m = new ValidationMessage();
+        if (missing.size() > 0) {
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		// TODO Auto-generated method stub
+            m.setSuccess(false);
+            m.setMessage("validation.notcompiled");
+        } else {
+
+            try {
+                boolean success = rule.evaluate(new HashMap<String,String>(), externals);
+            } catch (ScriptException e) {
+                m =generateParseProblemMessage( rule);
+                result.setSuccess(false);
+                
+
+            } catch (NullPointerException e) {
+                m =generateParseProblemMessage( rule);
+                result.setSuccess(false);
+                
+            }
+        }
+        result.addMessage(m);
+
+    }
+
+    private void evaluateColumns(ValidationResult result, Map<String, Map<String, String>> tests,
+            Map<String, String> externals, ValidationRule rule) {
+        ValidationMessage message = null;
+        // some validation problems have priority
+        boolean alreadyChecked = false;
+        for (String key : tests.keySet()) {
+            if (alreadyChecked) {
+                continue;
+            }
+
+            // get the map name ->value
+            Map<String, String> test = tests.get(key);
+            List<String> missing = checkRuleFields(rule.getVariables(), test);
+            // missing variables
+            if (missing.size() > 0) {
+                message = new ValidationMessage();
+                message.setMessage("validation.notcompiled");
+                message.setRule(rule);
+                message.setSuccess(false);
+                message.setElements(missing);
+                result.setSuccess(false);
+                alreadyChecked = true;
+                continue;
+            }
+
+            try {
+                boolean success = rule.evaluate(test, externals);
+                //
+                // check rules
+                //
+
+                // create the message if not created
+                if (message == null) {
+                    message = new ValidationMessage();
+                    message.setRule(rule);
+                    message.setSuccess(success);
+                    message.setMessage(rule.getError());
+                }
+                if (!success) {
+                    // add the columns that not satisfy the rule
+
+                    message.addElement(key);
+                    message.setSuccess(false);
+                    result.setSuccess(false);
+                }
+            } catch (ScriptException e) {
+                message= generateParseProblemMessage(rule);
+                result.setSuccess(false);
+                alreadyChecked = true;
+
+            } catch (NullPointerException e) {
+                message= generateParseProblemMessage(rule);
+                result.setSuccess(false);
+                alreadyChecked = true;
+            }
+
+        }
+        result.addMessage(message);
+
+    }
+
+    private void populateTests(List<Value> values, Map<String, Map<String, String>> tests) {
+        for (Value value : values) {
+            // for each column add a map name (1.2) -> value (e.g. 1234)
+            Map<String, String> testEntry;
+            String colname = value.getEntryItem().getColumnName();
+            if (!tests.containsKey(colname)) {
+                testEntry = new HashMap<String, String>();
+                tests.put(colname, testEntry);
+            } else {
+                testEntry = tests.get(colname);
+            }
+            testEntry.put(value.getEntryItem().getRowName(), value.getContent());
+        }
+    }
+
+    /**
+     * generates Parse problem message (utility subroutine)
+     * 
+     * @param message
+     * @param rule
+     */
+    private ValidationMessage generateParseProblemMessage(ValidationRule rule) {
+        ValidationMessage message = new ValidationMessage();
+        message.setMessage("validation.parseproblem");
+        message.setRule(rule);
+        message.setSuccess(false);
+        message.addElement(rule.getCondition());
+        return message;
+
+    }
+
+    /**
+     * Checks missing variables for a particular rule
+     * 
+     * @param rule the rule to check
+     * @param test the list of available variables
+     * @return the missing variables
+     */
+    private List<String> checkRuleFields(List<String> vars, Map<String, String> test) {
+        List<String> missing = new ArrayList<String>();
+
+        for (String name : vars) {
+
+            if (test.get(name) == null) {
+                missing.add(name);
+            }
+        }
+        return missing;
+    }
+
+    // Setters and Getters
+    public RuleList getRuleList() {
+        return ruleList;
+    }
+
+    public void setRuleList(RuleList ruleList) {
+        this.ruleList = ruleList;
+    }
+
+    // inizializer
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // TODO Auto-generated method stub
         InputStream rulesFile;
-		try {
-			rulesFile = applicationContext.getResource(
-					"classpath:validation-rules.xml").getInputStream();
-		} catch (IOException e) {
-			LOGGER.error("unable to load validation-rules:" + e);
+        try {
+            rulesFile = applicationContext.getResource("classpath:validation-rules.xml")
+                    .getInputStream();
+        } catch (IOException e) {
+            LOGGER.error("unable to load validation-rules:" + e);
             throw e;
-		}
+        }
 
-        if(rulesFile == null)
+        if (rulesFile == null)
             throw new IllegalArgumentException("rulesFile is null");
 
-//        if(! rulesFile.canRead() )
-//            throw new IllegalArgumentException("rulesFile does not exist: " + rulesFile);
+        // if(! rulesFile.canRead() )
+        // throw new IllegalArgumentException("rulesFile does not exist: " + rulesFile);
 
+        ruleList = JAXB.unmarshal(rulesFile, RuleList.class);
+        Iterator<ValidationRule> it = ruleList.iterator();
+        LOGGER.info(it.hasNext() + "");
+        while (it.hasNext()) {
+            LOGGER.info(it.next().getDescription());
 
-		ruleList = JAXB.unmarshal(rulesFile, RuleList.class);
-		Iterator<ValidationRule> it = ruleList.iterator();
-		LOGGER.info(it.hasNext() + "");
-		while (it.hasNext()) {
-			LOGGER.info(it.next().getDescription());
+        }
 
-		}
+    }
 
-	}
-
-	@Override
-	public void setApplicationContext(ApplicationContext ac)
-			throws BeansException {
-		this.applicationContext = ac;
-	}
+    @Override
+    public void setApplicationContext(ApplicationContext ac) throws BeansException {
+        this.applicationContext = ac;
+    }
 
 }
