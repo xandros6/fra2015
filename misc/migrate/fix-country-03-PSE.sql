f0f7778ead87f44bc618d19786588bc3093a7430
etj <etj@geo-solutions.it>
2013-08-14 17:02:35 +0200

    Adding PSE.

---
+++ misc/migrate/fix-country-03-PSE.sql
@@ -0,0 +1,7 @@
+-- Adding Occupied Palestinian territory
+
+insert into fra_country(id,iso3,countryarea,landarea,name,ctype) values (99900003, 'SSD', 602, 602, 'Occupied Palestinian territory', 'FRA');
+insert into fra_status (id, status) values (9996, 'empty');
+insert into fra_survey (id , country_id,status_id) values (9996, 99900003, 9996);
+
+
